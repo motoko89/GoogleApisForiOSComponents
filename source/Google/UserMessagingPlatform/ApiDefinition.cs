51db9461ccaf3fc2599cb29a6e571febc30d57bd
Hung H Ho <hohoanghung2010@gmail.com>
2020-10-05 22:21:02 -0700

    load sharpie

--- source/Google/UserMessagingPlatform/ApiDefinition.cs
+++ source/Google/UserMessagingPlatform/ApiDefinition.cs
@@ -1,66 +1,98 @@
-﻿using System;
-
+﻿using Foundation;
 using ObjCRuntime;
-using Foundation;
 using UIKit;
 
-namespace NativeLibrary
-{
-    // The first step to creating a binding is to add your native library ("libNativeLibrary.a")
-    // to the project by right-clicking (or Control-clicking) the folder containing this source
-    // file and clicking "Add files..." and then simply select the native library (or libraries)
-    // that you want to bind.
-    //
-    // When you do that, you'll notice that MonoDevelop generates a code-behind file for each
-    // native library which will contain a [LinkWith] attribute. VisualStudio auto-detects the
-    // architectures that the native library supports and fills in that information for you,
-    // however, it cannot auto-detect any Frameworks or other system libraries that the
-    // native library may depend on, so you'll need to fill in that information yourself.
-    //
-    // Once you've done that, you're ready to move on to binding the API...
-    //
-    //
-    // Here is where you'd define your API definition for the native Objective-C library.
-    //
-    // For example, to bind the following Objective-C class:
-    //
-    //     @interface Widget : NSObject {
-    //     }
-    //
-    // The C# binding would look like this:
-    //
-    //     [BaseType (typeof (NSObject))]
-    //     interface Widget {
-    //     }
-    //
-    // To bind Objective-C properties, such as:
-    //
-    //     @property (nonatomic, readwrite, assign) CGPoint center;
-    //
-    // You would add a property definition in the C# interface like so:
-    //
-    //     [Export ("center")]
-    //     CGPoint Center { get; set; }
-    //
-    // To bind an Objective-C method, such as:
-    //
-    //     -(void) doSomething:(NSObject *)object atIndex:(NSInteger)index;
-    //
-    // You would add a method definition to the C# interface like so:
-    //
-    //     [Export ("doSomething:atIndex:")]
-    //     void DoSomething (NSObject object, int index);
-    //
-    // Objective-C "constructors" such as:
-    //
-    //     -(id)initWithElmo:(ElmoMuppet *)elmo;
-    //
-    // Can be bound as:
-    //
-    //     [Export ("initWithElmo:")]
-    //     IntPtr Constructor (ElmoMuppet elmo);
-    //
-    // For more information, see https://aka.ms/ios-binding
-    //
+namespace Google.UserMessagingPlatform {
+	// @interface UMPDebugSettings : NSObject <NSCopying>
+	[BaseType (typeof (NSObject))]
+	interface UMPDebugSettings : INSCopying {
+		// @property (nonatomic) NSArray<NSString *> * _Nullable testDeviceIdentifiers;
+		[NullAllowed, Export ("testDeviceIdentifiers", ArgumentSemantic.Assign)]
+		string [] TestDeviceIdentifiers { get; set; }
+
+		// @property (nonatomic) UMPDebugGeography geography;
+		[Export ("geography", ArgumentSemantic.Assign)]
+		UMPDebugGeography Geography { get; set; }
+	}
+
+	// @interface UMPRequestParameters : NSObject
+	[BaseType (typeof (NSObject))]
+	interface UMPRequestParameters {
+		// @property (nonatomic) BOOL tagForUnderAgeOfConsent;
+		[Export ("tagForUnderAgeOfConsent")]
+		bool TagForUnderAgeOfConsent { get; set; }
+
+		// @property (copy, nonatomic) UMPDebugSettings * _Nullable debugSettings;
+		[NullAllowed, Export ("debugSettings", ArgumentSemantic.Copy)]
+		UMPDebugSettings DebugSettings { get; set; }
+	}
+
+	/*[Static]
+	[Verify (ConstantsInterfaceAssociation)]
+	partial interface Constants {
+		// extern NSString *const _Nonnull UMPVersionString;
+		[Field ("UMPVersionString", "__Internal")]
+		NSString UMPVersionString { get; }
+	}*/
+
+	// typedef void (^UMPConsentInformationUpdateCompletionHandler)(NSError * _Nullable);
+	delegate void UMPConsentInformationUpdateCompletionHandler ([NullAllowed] NSError arg0);
+
+	// @interface UMPConsentInformation : NSObject
+	[BaseType (typeof (NSObject))]
+	interface UMPConsentInformation {
+		// @property (readonly, nonatomic, class) UMPConsentInformation * _Nonnull sharedInstance;
+		[Static]
+		[Export ("sharedInstance")]
+		UMPConsentInformation SharedInstance { get; }
+
+		// @property (readonly, nonatomic) UMPConsentStatus consentStatus;
+		[Export ("consentStatus")]
+		UMPConsentStatus ConsentStatus { get; }
+
+		// @property (readonly, nonatomic) UMPConsentType consentType;
+		[Export ("consentType")]
+		UMPConsentType ConsentType { get; }
+
+		// @property (readonly, nonatomic) UMPFormStatus formStatus;
+		[Export ("formStatus")]
+		UMPFormStatus FormStatus { get; }
+
+		// -(void)requestConsentInfoUpdateWithParameters:(UMPRequestParameters * _Nullable)parameters completionHandler:(UMPConsentInformationUpdateCompletionHandler _Nonnull)handler;
+		[Export ("requestConsentInfoUpdateWithParameters:completionHandler:")]
+		void RequestConsentInfoUpdateWithParameters ([NullAllowed] UMPRequestParameters parameters, UMPConsentInformationUpdateCompletionHandler handler);
+
+		// -(void)reset;
+		[Export ("reset")]
+		void Reset ();
+	}
+
+	// typedef void (^UMPConsentFormLoadCompletionHandler)(UMPConsentForm * _Nullable, NSError * _Nullable);
+	delegate void UMPConsentFormLoadCompletionHandler ([NullAllowed] UMPConsentForm arg0, [NullAllowed] NSError arg1);
+
+	// typedef void (^UMPConsentFormPresentCompletionHandler)(NSError * _Nullable);
+	delegate void UMPConsentFormPresentCompletionHandler ([NullAllowed] NSError arg0);
+
+	// @interface UMPConsentForm : NSObject
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface UMPConsentForm {
+		// +(void)loadWithCompletionHandler:(UMPConsentFormLoadCompletionHandler _Nonnull)completionHandler;
+		[Static]
+		[Export ("loadWithCompletionHandler:")]
+		void LoadWithCompletionHandler (UMPConsentFormLoadCompletionHandler completionHandler);
+
+		// -(void)presentFromViewController:(UIViewController * _Nonnull)viewController completionHandler:(UMPConsentFormPresentCompletionHandler _Nullable)completionHandler;
+		[Export ("presentFromViewController:completionHandler:")]
+		void PresentFromViewController (UIViewController viewController, [NullAllowed] UMPConsentFormPresentCompletionHandler completionHandler);
+	}
+
+	/*[Static]
+	[Verify (ConstantsInterfaceAssociation)]
+	partial interface Constants {
+		// extern NSErrorDomain  _Nonnull const UMPErrorDomain;
+		[Field ("UMPErrorDomain", "__Internal")]
+		NSString UMPErrorDomain { get; }
+	}*/
 }
 
