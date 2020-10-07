1f7ce942fec3aba21b35d179010d7e999d515892
Hung H Ho <hohoanghung2010@gmail.com>
2020-10-06 21:35:10 -0700

    create async method

---
+++ source/Google/UserMessagingPlatform/Extensions.cs
@@ -0,0 +1,55 @@
+using Foundation;
+using System;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace Google.UserMessagingPlatform
+{
+	public partial class ConsentInformation
+	{
+		Task RequestConsentInfoUpdate(RequestParameters parameters)
+		{
+			var t = new TaskCompletionSource<bool>();
+			ConsentInformationUpdateCompletionHandler handler = (error) => {
+				if (error != null) {
+					t.TrySetException (new NSErrorException(error));
+				} else {
+					t.TrySetResult (true);
+				}
+			};
+			RequestConsentInfoUpdate (parameters, handler);
+			return t.Task;
+		}
+	}
+
+	public partial class ConsentForm
+	{
+		public static Task<ConsentForm> Load()
+		{
+			var t = new TaskCompletionSource<ConsentForm> ();
+			ConsentFormLoadCompletionHandler handler = (form, error) => {
+				if (error != null) {
+					t.TrySetException (new NSErrorException (error));
+				} else {
+					t.TrySetResult (form);
+				}
+			};
+			Load(handler);
+			return t.Task;
+		}
+
+		public Task Present(UIViewController viewController)
+		{
+			var t = new TaskCompletionSource<bool> ();
+			ConsentFormPresentCompletionHandler handler = (error) => {
+				if (error != null) {
+					t.TrySetException (new NSErrorException (error));
+				} else {
+					t.TrySetResult (true);
+				}
+			};
+			Present(viewController);
+			return t.Task;
+		}
+	}
+}
